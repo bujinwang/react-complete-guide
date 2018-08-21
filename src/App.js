471f53daef48e91515a71042d987d6411749b14b
Burt Wang <burtwang@universalrail.com>
2018-08-21 08:36:05 -0600

    lecture 64 psudo selector and Radium media selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -50,12 +51,17 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'yellow'
+      }
     };
 
     let persons = null;
 
     if (this.state.showPersons) {
+      style.backgroundColor = 'red';
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
@@ -70,21 +76,43 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+      style[":hover"]={
+        backgroundColor: 'lightred',
+        color: 'red'
+      }
     }
 
+    let classes = [];
+
+    if (this.state.persons.length <= 2){
+      classes.push('red'); // red
+    }
+
+    if(this.state.persons.length <= 1){
+      classes.push('bold'); // red, bold
+    }
+
+    if(this.state.persons.length <= 0){
+      classes.push('blank'); // red, bold
+    }
+
+
     return (
+    <StyleRoot>
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <h1>class name, </h1>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
         {persons}
       </div>
+    </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
