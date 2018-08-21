9e692b825b95119a54ecf5eaaa09ec6834ca7091
Burt Wang <burtwang@universalrail.com>
2018-08-21 08:45:07 -0600

    lecture 65 before eject

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Radium from 'radium'
+// import Radium from 'radium'
 import './Person.css';
 
 const person = (props) => {
-  const style = {
-    '@media (min-width: 500px)':{
-      width: '450px'
-    }
-  };
   return (
     <div className="Person" style={style}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
